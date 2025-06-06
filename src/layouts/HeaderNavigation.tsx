ab6feffb8d4201f4926e51d920744f41c89dcb31
steven-cl <castillsteven12@gmail.com>
2025-06-05 18:20:41 -0600

    refactor: update favicon and title in index.html, add HeaderNavigation component, and define image and media types

---
+++ src/layouts/HeaderNavigation.tsx
@@ -0,0 +1,25 @@
+import logo from '@assets/logo.png'
+import { Link } from 'react-router-dom'
+
+const HeaderNavigation = () => {
+    return (
+        <div className="bg-black navBar items-center p-4">
+            <div className="flex-1/4">
+                <Link to="/" className="flex flex-row justify-self-center ">
+                    <img src={logo} alt="Logo" className="h-10 logo" />
+                </Link>
+            </div>
+            <div className="flex-3/4">
+                <div className="flex flex-row items-center">
+                    <div className="flex flex-row flex-1/4 justify-end">
+                        <Link to="/" className="basis-1/12">Home</Link>
+                        <Link to="/about" className="basis-1/12">About</Link>
+                        <span className="basis-1/12"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderNavigation
---
+++ src/types/static.d.ts
@@ -0,0 +1,38 @@
+// Images
+declare module '*.png' {
+    const src: string
+    export default src
+}
+
+declare module '*.jpg' {
+    const src: string
+    export default src
+}
+
+declare module '*.jpeg' {
+    const src: string
+    export default src
+}
+
+declare module '*.gif' {
+    const src: string
+    export default src
+}
+
+declare module '*.svg' {
+    import * as React from 'react'
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+    const src: string
+    export default src
+}
+
+// Media
+declare module '*.mp3' {
+    const src: string
+    export default src
+}
+
+declare module '*.mp4' {
+    const src: string
+    export default src
+}
